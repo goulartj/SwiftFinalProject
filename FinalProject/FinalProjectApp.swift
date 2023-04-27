6e249957c3c3562c5cf9bb3fc2f4b5078b6d7407
John J Goulart <goulartj@bc.edu>
2023-04-27 18:54:23 -0400

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by John Goulart on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
